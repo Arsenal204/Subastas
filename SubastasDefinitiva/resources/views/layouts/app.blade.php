d20868899037ae8412c45561b316a36881540033
Sergiopm44 <sergiopinto664@gmail.com>
2025-03-10 15:46:37 +0100

    Navegacion

--- SubastasDefinitiva/resources/views/layouts/app.blade.php
+++ SubastasDefinitiva/resources/views/layouts/app.blade.php
@@ -11,16 +11,19 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Bootstrap CSS -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans antialiased bg-gradient-to-r from-blue-100 to-blue-300 dark:from-gray-800 dark:to-gray-900">
+        <div class="min-h-screen flex flex-col">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
+                <header class="bg-white dark:bg-gray-800 shadow-md rounded-lg mt-4 mx-4">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
@@ -28,10 +31,14 @@
             @endisset
 
             <!-- Page Content -->
-            <main>
-            @yield('content')
-
+            <main class="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                @yield('content')
             </main>
         </div>
+
+        <!-- Bootstrap JS and dependencies -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </body>
 </html>
